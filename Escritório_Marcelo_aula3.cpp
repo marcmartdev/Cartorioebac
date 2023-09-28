80199e257bdf140e4262d153f2fbd51d13d326d1
marcmartdev <145623928+marcmartdev@users.noreply.github.com>
2023-09-28 07:50:50 -0300

    Opção de saída
    Inserção da função "sair do sistema"

--- Escritório_Marcelo_aula3.cpp
+++ Escritório_Marcelo_aula3.cpp
@@ -122,6 +122,7 @@ int main()
 	 printf ("\t1 - Registrar nomes\n\n");
 	 printf ("\t2 - Consultar nomes\n\n");
 	 printf ("\t3 - Deletar nomes \n\n");
+	 printf ("\t4 -Sair do sistema\n\n");
 	 printf ("Opção: "); //final do menu
 	
 	 scanf("%d", &opcao); //armazenando a escolha do usuário
@@ -140,6 +141,11 @@ int main()
 		
 		case 3:
 		deletar();
+		break;
+		
+		case 4:
+		printf("Obrigado por utilizar o sistema!\n");
+		return 0;
 		break;
 		
 		default:
